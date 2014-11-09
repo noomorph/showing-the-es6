4ebe275a95c21ac1092c9442f38e16d87cfa0d97
Yaroslav Serhieiev <noomorph@gmail.com>
2014-11-09 20:26:11 +0200

    added a few slides

--- samples/00-const.js
+++ samples/00-const.js
@@ -1,12 +1,2 @@
-'use strict';
-
-const C = { value: 0 };
-C = "another value";
-
-try {
-} catch(e) {
-    print("cannot set another value");
-}
-
-C.value += 1;
-print(C.value);
+const C = { prop: 0 };
+C = { prop: 42 }; // ???
---
+++ slides/008-const.md
@@ -0,0 +1,13 @@
+[//]: # (templates/code.html)
+
+# **const** keyword
+
+What will happen?
+
+```
+samples/00-const.js
+```
+
+1. **Throws** exception?
+2. **Assigns** the value?
+3. **Ignores** assignment?
